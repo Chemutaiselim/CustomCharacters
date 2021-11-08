d6db8a44d5d521c43b68516331999e80d72924da
Selim <selimchemutai19@gmail.com>
2021-11-08 15:33:02 +0300

    Create main.c

---
+++ CustomCharacters_Code/CustomCharacters_Code/main.c
@@ -0,0 +1,18 @@
+/*
+ * CustomCharacters_Code.c
+ *
+ * Created: 08/11/2021 15:32:31
+ * Author : Default
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
