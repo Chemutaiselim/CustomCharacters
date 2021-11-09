5fb0ac07935ae1df13d6d18d8f28ba1ebf79e272
Selim <selimchemutai19@gmail.com>
2021-11-09 14:16:24 +0300

    Update main.c

--- CustomCharacters_Code/CustomCharacters_Code/main.c
+++ CustomCharacters_Code/CustomCharacters_Code/main.c
@@ -22,14 +22,22 @@ int main(void){
 	_delay_ms(500); 
 	LCD_Cmd(0xC0); /* Place cursor on the second row */ 
 	
-	const short Char5X8[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06};
-	for (int x=1; x<=sizeof(Char5X8); x++)
+	const short Char5X8[] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07};
+	for (int x=0; x < sizeof(Char5X8); x++)
 	{
 		LCD_Char(x);
-		LCD_String(" ");
+		if(x != 2){
+			LCD_String(" ");
+		}
+		
 		_delay_ms(100);
 		
 	}
+	for (int i = 0; i<16; i++)
+	{
+			LCD_Cmd(0x18);
+	}
+
 	
 	
 	
